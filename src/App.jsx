bd17884030e5349ee24bec4f1d8c3ca8cd15e781
MDAsimAkram <santoosanto27@gmail.com>
2023-03-26 23:00:05 +0600

    added header

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+     {/* App.jsx er sathe Header.jsx er connection ghotabo */}
+     <Header> </Header>
+    </div>
+  )
+}
+
+export default App
